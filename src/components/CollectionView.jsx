1880ba55e427a3b0df7c0be002e678f68cb7e7bf
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2026-02-01 16:01:49 +0100

    adjust margin on disclaimer text

--- src/components/CollectionView.jsx
+++ src/components/CollectionView.jsx
@@ -64,8 +64,8 @@ export default function CollectionView() {
         </Link>
       </div>
       <div className="collection_view">
-        <h1 style={{ color: "#03c8c8" }}>{collection.name}</h1>
-        <p style={{ color: "white" }}>{collection.description}</p>
+        <h1 style={{ margin: "20px", color: "#03c8c8" }}>{collection.name}</h1>
+        <p style={{ margin: "20px", color: "white" }}>{collection.description}</p>
 
         <CardsContainer>
           {collection.items.map((item) => (
